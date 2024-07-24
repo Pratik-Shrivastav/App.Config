61ee7d2a6fd4beb0070a8b6fbca4bc8371557f9e
Pratik.Shrivastav <Pratik.Shrivastav@DELL1615>
2024-07-24 14:39:16 +0530

    Add project files.

---
+++ App.Config/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int id = int.Parse(ConfigurationManager.AppSettings["id"]);
+        string name = ConfigurationManager.AppSettings["name"];
+        Console.WriteLine(id);
+        Console.WriteLine(name);
+    }
+}
